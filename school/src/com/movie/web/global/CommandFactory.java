94b77ca8ef0b2f1462fb02784e67be638d450e88
USER <hkim9414@naver.com>
2016-03-21 12:34:16 +0900

    ...

---
+++ school/src/com/movie/web/global/CommandFactory.java
@@ -0,0 +1,9 @@
+package com.movie.web.global;
+
+public class CommandFactory {
+	public Command getCommand(String a, String b) {
+		Command command = new Command(a, b);
+		
+		return command;
+	}
+}
--- school/src/com/movie/web/member/MemberController.java
+++ school/src/com/movie/web/member/MemberController.java
@@ -1,46 +1,42 @@
 package com.movie.web.member;
 
-import java.util.Scanner;
+import java.io.IOException;
 
-public class MemberController {
-	public static void main(String[] args) {
-		MemberService service = new MemberServiceImpl();
-		Scanner scanner = new Scanner(System.in);
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.movie.web.global.Command;
+import com.movie.web.global.CommandFactory;
+
+@WebServlet("/member/login_form.do")
+public class MemberController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	// 페이지 이동시에는 doGet
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
+		System.out.println("인덱스에서 들어옴");
+//		RequestDispatcher dis = request.getRequestDispatcher(new Command(doProc(request, response)[0], doProc(request, response)[1]).getView());
+		CommandFactory fac = new CommandFactory();
+		RequestDispatcher dis = request.getRequestDispatcher(fac.getCommand(doProc(request, response)[0], doProc(request, response)[1]).getView());
+		dis.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+	
+	protected String[] doProc(HttpServletRequest request, HttpServletResponse response) {
+		String[] arr = new String[2];
+		String path = request.getServletPath();
+		String temp = path.split("/")[2];
 		
-		while (true) {
-			System.out.print("[메뉴] 1.회원가입 2.로그인 3.내정보보기 4.내정보수정 5.탈퇴 0.종료 ");
-			switch (scanner.nextInt()) {
-			case 1:
-				System.out.print("아이디, 비밀번호, 이름, 주소, 생년월일을 입력하세요 ");
-				service.join(new MemberBean(scanner.next(), scanner.next(), scanner.next(), scanner.next(), scanner.nextInt()));
-				break;
-			case 2:
-				System.out.print("아이디와 비밀번호를 입력하세요 ");
-				System.out.println(service.login(scanner.next(), scanner.next()));
-				break;
-			case 3:
-				System.out.print("조회하려는 회원의 아이디를 입력하세요 ");
-				System.out.println(service.detail(scanner.next())); // service.detail(scanner.next()).toString()
-				break;
-			case 4:
-				System.out.print("수정하려는 회원의 아이디, 비밀번호, 주소를 입력하세요 ");
-				String id = scanner.next();
-				String name = service.detail(id).getName();
-				int birth = service.detail(id).getBirth();
-				System.out.println(service.update(new MemberBean(id, scanner.next(), name, scanner.next(), birth)));
-				break;
-			case 5:
-				System.out.print("삭제하려는 회원의 아이디를 입력하세요 ");
-				System.out.println(service.remove(scanner.next()));
-				break;
-			case 0:
-				System.out.println("프로그램 종료");
-				return;
-
-			default:
-				System.out.println("잘못 입력하셨습니다.");
-				return;
-			}
-		}
+		arr[0] = path.split("/")[1];
+//		arr[1] = temp3.split("\\.")[0];
+		arr[1] = temp.substring(0, temp.indexOf("."));
+		
+		return arr;
 	}
 }
